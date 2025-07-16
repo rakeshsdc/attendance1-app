1cc88ee9afee783e5f01f6e4a4109a545b747006
rakeshsdc <rakesh@sdcollege.in>
2025-07-16 16:14:39 +0530

    Update attendance_app_final2.py

--- attendance_app_final2.py
+++ attendance_app_final2.py
@@ -147,9 +147,9 @@ if st.session_state.role in ["teacher", "admin", "dept_admin"]:
                 updated_status[row["student_id"]] = status
 
         if st.button("✅ Submit Attendance"):
-            new_data = []
-            for sid, status in updated_status.items():
-            new_data.append({
+    new_data = []
+    for sid, status in updated_status.items():
+        new_data.append({
             "date": selected_date,
             "hour": selected_hour,
             "course_id": selected_course,
@@ -159,6 +159,7 @@ if st.session_state.role in ["teacher", "admin", "dept_admin"]:
             "extra_time": extra_time,
             "duration": duration
         })
+
 
     new_df = pd.DataFrame(new_data)
 
